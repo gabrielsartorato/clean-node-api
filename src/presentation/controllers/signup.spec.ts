a393d981d08a1df4151051c9fe8f25dece73ac45
Gabriel Sartorato <gabriel.sartorato@hotmail.com>
2020-10-28 22:23:27 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignController } from './signUp';
+
+describe('SignUp Controller', () => {
+  it('should return 400 if no name is provide', () => {
+    const sut = new SignController();
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        password_confirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
