6a5df8c92f27a12eb9ff3e5bfd4db8da35054523
Gabriel Sartorato <gabriel.sartorato@hotmail.com>
2020-11-03 10:51:44 -0300

    feat: ensure MongoDb recconect if client is down

--- src/infra/db/mongodb/account-repository/account-repository.ts
+++ src/infra/db/mongodb/account-repository/account-repository.ts
@@ -4,7 +4,7 @@ import { AddAccountModel } from '../../../../domain/usecases/add-account';
 import { MongoHelper } from '../helpers/mongo-helper';
 export class AccountMongoRepository implements AddAccountRepository {
   async add({ name, email, password }: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts');
+    const accountCollection = await MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne({ name, email, password });
 
     return MongoHelper.map(result.ops[0]);
